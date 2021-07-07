dc02c80dee466eac4726eced90a7b1fad3105566
Dorokhov Alexander <svdfold@gmail.com>
2021-07-07 18:02:48 +0300

    Initial Commit

---
+++ HW2.1 Part 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1 Part 2
+//
+//  Created by Mike Albright on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
